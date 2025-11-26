3db8f4b811cfacb261837a41a5722c56e160cec3
shola-devv <creekemmanuelf@gmail.com>
2025-11-26 02:55:27 +0100

    added the plan protection 2

--- app/home/upgrade/page.tsx
+++ app/home/upgrade/page.tsx
@@ -10,17 +10,21 @@ export default function UpgradePage() {
 
 
   return (
-    <div className="p-6 max-w-xl mx-auto">
-      {restricted && (
-        <div className="mb-4 text-sm border border-red-300 bg-red-50 text-red-700 p-3 rounded-lg">
-          Feature not available for free plan.<span className="underline cursor-pointer" onClick={() => router.push("/home/subscribe")}> Upgrade </span>your plan to get access.
-        </div>
-      )}
-
-   
-      
-      {/* Your upgrade pricing UI here */}
-
+     <div className="min-h-screen flex items-center justify-center p-4">
+      <div className="w-full max-w-xl mx-auto">
+        {restricted && (
+          <div className="text-center text-sm border border-red-300 bg-red-50 text-red-700 p-4 rounded-lg">
+            Feature not available for free plan.{" "}
+            <span
+              className="underline cursor-pointer font-medium"
+              onClick={() => router.push("/home/subscribe")}
+            >
+              Upgrade
+            </span>{" "}
+            your plan to get access.
+          </div>
+        )}
+      </div>
     </div>
   );
 }
