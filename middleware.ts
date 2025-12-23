9c6100dacafe88732d15df97b564d5db75790068
shola-devv <creekemmanuelf@gmail.com>
2025-12-23 04:59:30 +0100

    vd 21

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { getToken } from "next-auth/jwt"
-import { NextResponse } from "next/server"
+import { NextResponse, type NextRequest } from "next/server"
 
-export async function middleware(req) {
+export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
 
