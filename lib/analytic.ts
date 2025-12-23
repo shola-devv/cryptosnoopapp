9fa95dc70c0bcc2dc4445cfbed0073042f1dbc07
shola-devv <creekemmanuelf@gmail.com>
2025-12-23 04:32:02 +0100

    vd 18

--- lib/analytic.ts
+++ lib/analytic.ts
@@ -1,6 +1,6 @@
 // lib/analytics.ts
 export const pageview = (url: string) => {
-  if (typeof window !== "undefined" && window.gtag) {
+  if (typeof window !== "undefined" && typeof window.gtag === 'function') {
     window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
       page_path: url,
     });
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,8 @@
 declare global {
   interface Window {
     ethereum?: any
+    // Google Analytics gtag function
+    gtag?: (...args: any[]) => void
   }
 }
 
